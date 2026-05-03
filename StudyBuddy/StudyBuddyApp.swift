7bcbfc78ba31aaaf851e37120536256b99e5f123
ina927 <inasong03@gmail.com>
2026-05-03 16:36:34 +1000

    Initial commit

---
+++ StudyBuddy/StudyBuddyApp.swift
@@ -0,0 +1,17 @@
+//
+//  StudyBuddyApp.swift
+//  StudyBuddy
+//
+//  Created by Ina Song on 3/5/2026.
+//
+
+import SwiftUI
+
+@main
+struct StudyBuddyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
